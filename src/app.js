d338c514bc32a1a0256d5b238cae3e0db101bd9b
Christine <christine.altmann@gmx.com>
2019-09-10 16:04:08 +0100

    trial test to check travis is happy

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const exphbs = require("express-handlebars");
 const path = require("path");
 
 const bodyParser = require("body-parser");
-const controllers = require("./controllers/index")
+const controllers = require("./controllers/index");
 
 const app = express();
 
@@ -13,21 +13,25 @@ app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
-app.engine("hbs", exphbs({
+app.engine(
+  "hbs",
+  exphbs({
     extname: "hbs",
     layoutsDir: path.join(__dirname, "views", "layouts"),
     partialsDir: path.join(__dirname, "views", "partials"),
     defaultLayout: "main"
-}));
+  })
+);
 
 app.set("port", process.env.PORT || 3000);
 app.use(controllers);
 
-
 // hypothetical test
 const dbTest = (a, b) => {
-    return a + b;
-}
+  return a + b;
+};
 
-module.exports = dbTest;
-module.exports = app;
+module.exports = {
+  dbTest,
+  app
+};
--- src/tests/routes.test.js
+++ src/tests/routes.test.js
@@ -1,5 +1,5 @@
-const router = require('../app');
+const { dbTest } = require("../app");
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(router(1, 2)).toBe(3);
+test("adds 1 + 2 to equal 3", () => {
+  expect(dbTest(1, 2)).toBe(3);
 });
