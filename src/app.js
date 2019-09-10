ba3634eb1ba9c12941e34c7f24cc37ccf387264a
Christine <christine.altmann@gmx.com>
2019-09-10 12:39:54 +0100

    setting up express app

--- src/app.js
+++ src/app.js
@@ -1,5 +1,33 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const path = require("path");
+
+const bodyParser = require("body-parser");
+const controllers = require("./controllers/index")
+
+const app = express();
+
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+app.engine("hbs", exphbs({
+    extname: "hbs",
+    layoutsDir: path.join(__dirname, "views", "layouts"),
+    partialsDir: path.join(__dirname, "views", "partials"),
+    defaultLayout: "main"
+}));
+
+app.set("port", process.env.PORT || 3000);
+app.use(controllers);
+
+
+// hypothetical test
 const dbTest = (a, b) => {
     return a + b;
 }
 
 module.exports = dbTest;
+module.exports = app;
