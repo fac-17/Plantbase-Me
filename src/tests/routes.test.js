c75f8c7700c78452cd3466529b894315da73662a
Gregor <gregorlthomson@sky.com>
2019-09-11 11:28:19 +0100

    Tweaking routes tests Relates #60

--- src/tests/routes.test.js
+++ src/tests/routes.test.js
@@ -1,14 +1,29 @@
 const request = require("supertest");
 const test = require("tape");
-const express = require("express");
 const app = require("../app");
 
 test("Home route should return HTML content", t => {
   request(app)
     .get("/")
-    .expect("Content-Type", /html/)
+    .expect("content-type", /html/)
     .end((err, res) => {
-      t.equal(1, 1);
+      t.equal(
+        res.headers["content-type"],
+        "text/html; charset=utf-8",
+        "Content type is html"
+      );
+      t.error(err, "There is no error");
+      t.end();
+    });
+});
+
+test("Home route should return with statuscode 200", t => {
+  request(app)
+    .get("/")
+    .expect(200)
+    .end((err, res) => {
+      t.equal(res.status, 200, "Status code is 200");
+      t.error(err, "There is no error");
       t.end();
     });
 });
