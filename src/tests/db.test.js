2e4a631aa00ac391bf38d0e239ac99f060e2e4d0
Christine <christine.altmann@gmx.com>
2019-09-12 10:26:00 +0100

    finish database testing set up

--- src/tests/db.test.js
+++ src/tests/db.test.js
@@ -2,9 +2,18 @@ const tape = require("tape");
 const _test = require("tape-promise").default;
 
 const test = _test(tape);
-const { build } = require("../model/database/db_build");
+const { build, connection } = require("../model/database/db_build");
 
 test("tape test for db files is working", t => {
   t.equal(1, 1, "1 equals 1");
   t.end();
 });
+
+test("Check database builds", t => {
+  buildDb((err, res) => {
+    t.error(err, "there is no error");
+    t.end();
+  });
+});
+
+// ideally would be good to be able to check whether a database actually has built itself
