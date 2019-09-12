0a00a8fb35420ca0e5ac8a20e37e5f131dbb38ec
Gregor <gregorlthomson@sky.com>
2019-09-12 12:52:55 +0100

    Route product page and correct names Relates #64 , #70

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 const cravingIcons = ["hamburger", "cheese", "jelly"];
-const productResult = [
+const allProducts = [
   {
     product_name: "Violife Original Grated",
     product_description:
@@ -13,12 +13,25 @@ const productResult = [
   }
 ];
 
+const chosenProduct = {
+  product_name: "Violife Original Grated",
+  product_description:
+    "With coconut oil and vitamin B12. Melts great! Free from dairy, soya, gluten, lactose and nuts. No preservatives. Suitable for vegans & vegetarians. Kosher",
+  product_price: 2.49,
+  product_image: "https://imgur.com/ZCAQAGA.jpg",
+  where_to_buy: ["Waitrose", "Asda", "Morrisons"]
+};
+
 router.get("/", (req, res) => {
   res.render("home", { cravingIcons });
 });
 
 router.get("/results", (req, res) => {
-  res.render("results", { productResult });
+  res.render("results", { allProducts });
+});
+
+router.get("/product-page", (req, res) => {
+  res.render("productPage", { chosenProduct });
 });
 
 module.exports = router;
