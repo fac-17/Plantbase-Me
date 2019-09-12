a065905c29ae92697ae7479dbdcdae7c27b15cbe
Emmanuel <emanagyei@gmail.com>
2019-09-12 13:02:28 +0100

    seperation of concerns for queries Relates #58

--- src/model/queries/index.js
+++ src/model/queries/index.js
@@ -1,12 +1,5 @@
-const connection = require("../database/db_connection");
-
-const getCravings = () => connection.query("SELECT * FROM cravings");
-
-const getProductsbyCraving = cravingId => {
-  return connection.query("SELECT * FROM products WHERE craving_id = $1", [
-    cravingId
-  ]);
-};
+const getCravings = require("./getCravings");
+const getProductsbyCraving = require("./getProductsByCraving");
 
 module.exports = {
   getCravings,
