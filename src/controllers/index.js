e3da92eb7967494661942db2b8822f59ab2d641a
Gregor <gregorlthomson@sky.com>
2019-09-12 12:17:05 +0100

    Add placeholder data and correct image path Relates #64 , 68

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -3,6 +3,15 @@ const express = require("express");
 const router = express.Router();
 
 const cravingIcons = ["hamburger", "cheese", "jelly"];
+const productResult = [
+  {
+    product_name: "Violife Original Grated",
+    product_description:
+      "With coconut oil and vitamin B12. Melts great! Free from dairy, soya, gluten, lactose and nuts. No preservatives. Suitable for vegans & vegetarians. Kosher",
+
+    product_image: "https://imgur.com/ZCAQAGA.jpg"
+  }
+];
 
 router.get("/", (req, res) => {
   res.render("home", { cravingIcons });
