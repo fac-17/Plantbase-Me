2701d0b38b63ee114b208431a488237fdd600402
Sam Harris <sam@jam-bc.com>
2019-09-12 10:58:07 +0100

    svgs from assets loading on homepage relates #61

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const cravingIcons = ["burger", "cheese", "jelly"];
+const cravingIcons = ["hamburger", "cheese", "jelly"];
 
 router.get("/", (req, res) => {
   res.render("home", { cravingIcons });
