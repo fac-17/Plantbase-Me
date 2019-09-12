187583f31c1445d761aaf544701cf01817d5def6
Sam Harris <sam@jam-bc.com>
2019-09-12 10:24:49 +0100

    svg partials added and showing in header reltes #59

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.render("home");
+  res.render("home");
 });
 
 module.exports = router;
