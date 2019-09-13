39efde2749cde1c58d851092297b5dc70969a8b7
Sam Harris <sam@jam-bc.com>
2019-09-13 12:56:11 +0100

    fix query to get all products by a craving

--- src/model/queries/getProductsByCraving.js
+++ src/model/queries/getProductsByCraving.js
@@ -1,7 +1,8 @@
 const connection = require("../database/db_connection");
 
 module.exports = cravingId => {
-  return connection.query("SELECT * FROM products WHERE craving_id = $1", [
-    cravingId
-  ]);
+  return connection.query(
+    "SELECT id, product_name, product_description, product_image FROM products WHERE craving_id = $1",
+    [cravingId]
+  );
 };
