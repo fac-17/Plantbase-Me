cf081c68e5965d2cc21678b177b0564de78cc774
Sam Harris <sam@jam-bc.com>
2019-09-13 14:34:54 +0100

    add rating to products shown when search by craving

--- src/model/queries/getProductsByCraving.js
+++ src/model/queries/getProductsByCraving.js
@@ -1,8 +1,22 @@
 const connection = require("../database/db_connection");
 
-module.exports = cravingId => {
-  return connection.query(
-    "SELECT id, product_name, product_description, product_image FROM products WHERE craving_id = $1",
+// module.exports = cravingId => {
+//   return connection.query(
+//     "SELECT id, product_name, product_description, product_image FROM products WHERE craving_id = $1",
+//     [cravingId]
+//   );
+// };
+
+module.exports = cravingId =>
+  connection.query(
+    `SELECT p.id, product_name, product_description, product_image, product_avg_rating
+    FROM products p
+    JOIN (
+        SELECT product_id, AVG(rating) AS product_avg_rating
+        FROM ratings
+        GROUP BY product_id
+    ) AS avg_ratings
+    ON avg_ratings.product_id = p.id
+    WHERE p.craving_id = $1`,
     [cravingId]
   );
-};
