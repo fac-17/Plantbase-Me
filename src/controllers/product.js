3358ce02960cf275e365f861d15d73589833636f
Sam Harris <sam@jam-bc.com>
2019-09-13 14:53:56 +0100

    product page shows details about product

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -3,6 +3,10 @@ const { getProductById } = require("../model/queries");
 exports.get = (req, res, next) => {
   const { id } = req.params;
   getProductById(id)
-    .then(chosenProduct => res.render("productPage", { chosenProduct }))
+    .then(product => {
+      const chosenProduct = product[0];
+      console.log("chosen Product =", chosenProduct);
+      res.render("productPage", { chosenProduct });
+    })
     .catch(err => next(err));
 };
