36fb0a6122557386cdbeed9bc64e9d4cc3e621a1
Christine <christine.altmann@gmx.com>
2019-09-16 19:35:32 +0100

    rotation working theoretically but wrong axis

--- public/js/dom.js
+++ public/js/dom.js
@@ -3,3 +3,26 @@ const logo = document.getElementById("logo");
 logo.addEventListener("click", () => {
   location.assign("/");
 });
+
+// slider on hp
+var contentSlider = document.querySelector(".content-slider");
+var slideCount = 4;
+var selectedIndex = 0;
+
+function rotateCarousel() {
+  var angle = (selectedIndex / slideCount) * -360;
+  contentSlider.style.transform =
+    "translateZ(-256px) rotateY(" + angle + "deg)";
+}
+
+var prevButton = document.querySelector(".previous-button");
+prevButton.addEventListener("click", function() {
+  selectedIndex--;
+  rotateCarousel();
+});
+
+var nextButton = document.querySelector(".next-button");
+nextButton.addEventListener("click", function() {
+  selectedIndex++;
+  rotateCarousel();
+});
