0b9b80b29081943d1437ad29bda68a6214335fa0
Emmanuel <emanagyei@gmail.com>
2019-09-16 12:40:49 +0100

    add controllers for about us page Relates #72

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -5,12 +5,14 @@ const home = require("./home");
 const allResults = require("./allResults");
 const cravingResults = require("./cravingResults");
 const product = require("./product");
+const aboutUs = require("./aboutUs");
 const error = require("./error");
 
 router.get("/", home.get);
 router.get("/results", allResults.get);
 router.get("/results/:craving", cravingResults.get);
 router.get("/product/:id", product.get);
+router.get("/aboutUs", aboutUs.get);
 
 router.use(error.client);
 router.use(error.server);
