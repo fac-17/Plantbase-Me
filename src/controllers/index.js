2adcb8858f7f82fc614fcae614480a7156338e3e
Gregor <gregorlthomson@sky.com>
2019-09-16 12:44:39 +0100

    Route thanks for submission page Relates #119

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -5,13 +5,14 @@ const home = require("./home");
 const allResults = require("./allResults");
 const cravingResults = require("./cravingResults");
 const product = require("./product");
+const thanksForSubmission = require("./thanksForSubmission");
 const error = require("./error");
 
 router.get("/", home.get);
 router.get("/results", allResults.get);
 router.get("/results/:craving", cravingResults.get);
 router.get("/product/:id", product.get);
-
+router.get("/thanksforsubmission", thanksForSubmission.get);
 router.use(error.client);
 router.use(error.server);
 
