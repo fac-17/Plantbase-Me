57047ebff0f4a52a4a3d9250a1c39fb64e41eece
Gregor <gregorlthomson@sky.com>
2019-09-16 14:57:45 +0100

    Remove tags from query Relates #116

--- src/model/queries/postRatingComment.js
+++ src/model/queries/postRatingComment.js
@@ -1,9 +1,9 @@
 const connection = require("../database/db_connection");
 
 module.exports = rating_comment => {
-  const { product_id, rating, comment, tags, votes } = rating_comment;
+  const { product_id, rating, comment, votes } = rating_comment;
   return connection.query(
     "INSERT INTO ratings (product_id, rating, comment, tags, votes) VALUES ($1, $2, $3, $4, $5)",
-    [product_id, rating, comment, tags, votes]
+    [product_id, rating, comment, votes]
   );
 };
