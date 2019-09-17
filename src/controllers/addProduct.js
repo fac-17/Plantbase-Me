40f5c28789baaa521c7d1737099f6a45078d4ae0
Gregor <gregorlthomson@sky.com>
2019-09-17 14:24:34 +0100

    Correct path for adding a product Relates #141

--- src/controllers/addProduct.js
+++ src/controllers/addProduct.js
@@ -1,12 +1,12 @@
 const { postProduct } = require("../model/queries");
 
 exports.post = (req, res) => {
-  const { product } = req.body;
+  console.log(req.body);
   postProduct(product, (err, res) => {
     if (err) console.log(err);
     else {
       //redirect to thanks for submissions page
-      res.redirect(`/src/views/thanksForSubmission.hbs`);
+      res.redirect(`/thanksforsubmission`);
     }
   });
 };
