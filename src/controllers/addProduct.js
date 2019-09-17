9928cf98d7f37d487fb09d1e3baf50bda1da88c8
Gregor <gregorlthomson@sky.com>
2019-09-17 16:23:15 +0100

    Deconstruct data for passing to separate functions Relates #148

--- src/controllers/addProduct.js
+++ src/controllers/addProduct.js
@@ -2,6 +2,9 @@ const { postProduct } = require("../model/queries");
 
 exports.post = (req, res) => {
   const product = req.body;
+  const { product_name, rating, comment } = product;
+  delete product.rating;
+  delete product.comment;
   postProduct(product);
   res.redirect("/thanksforsubmission");
 };
