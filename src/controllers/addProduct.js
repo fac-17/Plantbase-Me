6da6876342eb364d2c6514432f0495ee6d92b163
Gregor <gregorlthomson@sky.com>
2019-09-17 14:41:33 +0100

    Implement post to database Relates #141

--- src/controllers/addProduct.js
+++ src/controllers/addProduct.js
@@ -1,12 +1,7 @@
 const { postProduct } = require("../model/queries");
 
 exports.post = (req, res) => {
-  console.log(req.body);
-  postProduct(product, (err, res) => {
-    if (err) console.log(err);
-    else {
-      //redirect to thanks for submissions page
-      res.redirect(`/thanksforsubmission`);
-    }
-  });
+  const product = req.body;
+  postProduct(product);
+  res.redirect("/thanksforsubmission");
 };
