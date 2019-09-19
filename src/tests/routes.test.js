69ced31f6671d576b1d5d6300734b61afde4ee18
Christine <christine.altmann@gmx.com>
2019-09-19 16:17:19 +0100

    router test for our mission page

--- src/tests/routes.test.js
+++ src/tests/routes.test.js
@@ -104,3 +104,18 @@ test("How to contribute Page gives correct content type and status 200", t => {
       t.end();
     });
 });
+
+test("How to contribute Page gives correct content type and status 200", t => {
+  request(app)
+    .get("/ourmission")
+    .end((err, res) => {
+      t.equal(
+        res.headers["content-type"],
+        "text/html; charset=utf-8",
+        "Content type is HTML"
+      );
+      t.equal(res.status, 200, "Status code is 200");
+      t.error(err, "There is no error");
+      t.end();
+    });
+});
