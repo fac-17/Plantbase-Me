b7a5f49d457f40e66460d967c2c5353c3455c723
Sam Harris <sam@jam-bc.com>
2019-09-19 12:52:38 +0100

    refactor home page code relates #201

--- public/js/home.js
+++ public/js/home.js
@@ -1,71 +1,48 @@
 // slider on home-page
 const contentSlider = document.querySelector(".content-slider");
-const slideCount = 4;
-let selectedIndex = 4;
+const numOfSlides = 4;
+let cravingSelectedIndex = 1;
+let slideIndex = 0;
 
-function rotateCarousel() {
-  // weird next line is necessary because for whatever reason we're counting up from 4?
-  var angle = (selectedIndex / slideCount) * -360 + 360;
-  contentSlider.style.transform =
-    "translateZ(-256px) rotateY(" + angle + "deg)";
-}
-
-let prevButtonCount = 0;
-
-var prevButton = document.querySelector(".previous-button");
-if (prevButton) {
-  prevButton.addEventListener("click", function() {
-    selectedIndex--;
-    prevButtonCount++;
-    console.log("prev", prevButtonCount);
-    // limitSelectedIndex();
-    rotateCarousel();
-  });
-  console.log("selectedIndex", selectedIndex);
-  console.log("slideCount", slideCount);
-}
-
-var nextButton = document.querySelector(".next-button");
-if (nextButton) {
-  nextButton.addEventListener("click", function() {
-    selectedIndex++;
-    prevButtonCount--;
-    console.log("selectedIndex", selectedIndex);
-    console.log("slideCount", slideCount);
-    // limitSelectedIndex();
-    rotateCarousel();
-  });
-}
+const updateIndexes = direction => {
+  if (direction === "forward") {
+    slideIndex++;
+    cravingSelectedIndex++;
+  } else {
+    slideIndex--;
+    cravingSelectedIndex--;
+  }
+  limitSelectedIndex();
+};
 
 const limitSelectedIndex = () => {
-  if (selectedIndex > slideCount) {
-    selectedIndex = 1;
-  } else if (selectedIndex <= 0) {
-    selectedIndex = slideCount;
+  if (cravingSelectedIndex === 5) {
+    cravingSelectedIndex = 1;
+  } else if (cravingSelectedIndex <= 0) {
+    cravingSelectedIndex = 4;
   }
 };
 
-// Pass selected category to the back-end
+const rotateCarousel = direction => {
+  updateIndexes(direction);
+  const angle = (slideIndex / numOfSlides) * -360;
+  contentSlider.style.transform =
+    "translateZ(-256px) rotateY(" + angle + "deg)";
+};
 
+const loadSelectedCraving = () =>
+  location.assign(`/results/${cravingSelectedIndex}`);
+
+// Rotate slider on forward / backward clicks
+const prevButton = document.querySelector(".previous-button");
+prevButton.addEventListener("click", () => rotateCarousel("backward"));
+
+const nextButton = document.querySelector(".next-button");
+nextButton.addEventListener("click", () => rotateCarousel("forward"));
+
+// render results of selected craving on button or icon click
 const selectButton = document.querySelector(".select-button");
+selectButton.addEventListener("click", () => loadSelectedCraving());
 
-// resets index to a range that corresponds with a page
-const changeSelectedIndex = () => {
-  if (selectedIndex > 7) {
-    selectedIndex = (selectedIndex % 4) + 1;
-  }
-  if (selectedIndex < 4) {
-    console.log("minus stuff", (selectedIndex = 7 - (selectedIndex % 4)));
-    selectedIndex = 7 - (selectedIndex % 4);
-  }
-};
-
-if (selectButton) {
-  selectButton.addEventListener("click", () => {
-    changeSelectedIndex();
-    console.log("selectedIndex2", selectedIndex);
-    // selectedIndex++;
-    // limitSelectedIndex();
-    location.assign(`/results/${selectedIndex - 3}`);
-  });
-}
+const selectedSlide = document.querySelector(".content-slider");
+selectedSlide.addEventListener("click", () => loadSelectedCraving());
