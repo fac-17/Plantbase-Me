bcdccb2ed81d9ccece0eac4c9622af5610755869
Christine <christine.altmann@gmx.com>
2019-09-20 00:54:03 +0100

    amend postProduct sql query

--- src/model/queries/postProduct.js
+++ src/model/queries/postProduct.js
@@ -11,14 +11,14 @@ module.exports = product => {
   } = product;
   where_to_buy = '{"' + where_to_buy + '"}';
   return connection.query(
-    "INSERT INTO products (product_name, product_description, where_to_buy, product_image, craving_id, product_price)VALUES($1, $2, $3, $4, $5, $6)",
+    "INSERT INTO products (product_name, product_description, craving_id, where_to_buy, product_image, product_price) VALUES($1, $2, $3, $4, $5, $6) RETURNING id",
     [
-      product_name,
+      (product_name,
       product_description,
       where_to_buy,
       product_image,
       craving_id,
-      product_price
+      product_price)
     ]
   );
 };
