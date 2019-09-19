12773e38fce127693299cf0c5c70da44aaf6d0a4
Gregor <gregorlthomson@sky.com>
2019-09-19 12:58:36 +0100

    Add test for allCravings query Relates #192

--- src/tests/db.test.js
+++ src/tests/db.test.js
@@ -10,9 +10,8 @@ test("Tape test for db files is working", t => {
   t.end();
 });
 
-// ideally would be good to be able to check whether a database actually has built itself
-
 test("Check test database builds correctly", t => {
+  t.equal(process.env.NODE_ENV, "test", "Node environment is test");
   connection
     .query(build)
     .then(() => console.log("Database connected and built"))
@@ -21,4 +20,18 @@ test("Check test database builds correctly", t => {
       t.end();
     })
     .catch(e => console.error("error", e));
+});
+
+test("Check results are correct from getCravings query", t => {
+  const expected = [
+    { id: 1, craving_name: "Cheese" },
+    { id: 2, craving_name: "Meat" },
+    { id: 3, craving_name: "Milk" },
+    { id: 4, craving_name: "Sweets" }
+  ];
+
+  queries.getCravings().then(allCravings => {
+    t.deepEqual(allCravings, expected, "Correct cravings are returned");
+    t.end();
+  });
 });
